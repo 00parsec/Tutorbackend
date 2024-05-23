6386eba730c13dacdb01c70dfa160c49f43e98e9
00parsec <137364055+00parsec@users.noreply.github.com>
2024-05-23 18:15:27 -0400

    Update app.js

--- app.js
+++ app.js
@@ -6,7 +6,8 @@ const { Sequelize } = require('sequelize');
 require('dotenv').config();
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+//const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 10000
 
 app.use(bodyParser.json());
 app.use(cors());
