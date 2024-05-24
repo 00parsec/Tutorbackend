ad4395c935ca0fa7a63fdc963fe3f4dd64a6404c
00parsec <137364055+00parsec@users.noreply.github.com>
2024-05-24 00:22:12 -0400

    Update app.js

--- app.js
+++ app.js
@@ -1,3 +1,58 @@
+
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const { Sequelize } = require('sequelize');
+const routes = require('./routes');
+
+require('dotenv').config();
+
+const app = express();
+const port = process.env.PORT || 10000;
+
+app.use(bodyParser.json());
+app.use(cors());
+
+// Configuración de Sequelize
+const sequelize = new Sequelize({
+  database: process.env.DB_NAME,
+  username: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  dialect: 'postgres',
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false
+    }
+  }
+});
+
+// Verificar la conexión a la base de datos
+sequelize.authenticate()
+  .then(() => {
+    console.log('Connection to the database has been established successfully.');
+    return sequelize.sync();
+  })
+  .then(() => {
+    console.log('Database synced');
+    app.listen(port, '0.0.0.0', () => {
+      console.log(`Server running on port ${port}`);
+    });
+  })
+  .catch((error) => {
+    console.error('Unable to connect to the database:', error);
+  });
+
+// Uso de las rutas definidas
+app.use('/', routes);
+
+module.exports = app;
+
+
+/*
+--2
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
@@ -42,8 +97,10 @@ sequelize.sync().then(() => {
 
 module.exports = app;
 
+*/
 
 /*
+--1
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
