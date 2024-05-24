3eba9c3a1d465cdb9792f1e74183fff612bd1c5c
00parsec <137364055+00parsec@users.noreply.github.com>
2024-05-24 02:26:18 -0400

    Update database.js

--- database.js
+++ database.js
@@ -3,8 +3,13 @@ require('dotenv').config();
 
 const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
   host: process.env.DB_HOST,
-  port: process.env.DB_PORT,
+  port: process.env.DBPORT,
   dialect: 'postgres'
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false
+    }
 });
 
 module.exports = { sequelize };
