18291ac365627108b821602a26e62eaec5222b14
00parsec <137364055+00parsec@users.noreply.github.com>
2024-05-24 01:46:16 -0400

    Update app.js

--- app.js
+++ app.js
@@ -19,8 +19,8 @@ const sequelize = new Sequelize(process.env.DATABASE_URL, {
   dialect: 'postgres',
   dialectOptions: {
     ssl: {
-      require: false,
-      rejectUnauthorized: false
+      require: true,
+      rejectUnauthorized: true
     }
   }
 });
