aee2ba734ae0541fa64cbdeb14f4bcba5d9bcaac
00parsec <137364055+00parsec@users.noreply.github.com>
2024-05-24 01:42:13 -0400

    Update authController.js

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,29 +1,4 @@
 
-const db = require('../db'); // Importar la conexión a la base de datos
-
-exports.register = async (req, res) => {
-  try {
-    const { username, password, email, firstName, lastName, phoneNumber, role } = req.body;
-    
-    // Verificar si el nombre de usuario ya existe en la base de datos
-    const existingUser = await db.query('SELECT * FROM users WHERE username = $1', [username]);
-    if (existingUser.rows.length > 0) {
-      return res.status(400).json({ message: 'Username already exists' });
-    }
-
-    // Insertar el nuevo usuario en la base de datos
-    const hashedPassword = await bcrypt.hash(password, 10);
-    await db.query('INSERT INTO users (username, password, email, firstName, lastName, phoneNumber, role) VALUES ($1, $2, $3, $4, $5, $6, $7)',
-      [username, hashedPassword, email, firstName, lastName, phoneNumber, role]);
-
-    res.status(201).json({ message: 'User registered successfully' });
-  } catch (error) {
-    console.error(error);
-    res.status(500).json({ message: 'Error registering user' });
-  }
-};
-
-/*
 const User = require('../models/Users'); 
 const jwt = require('jsonwebtoken');
 const bcrypt = require('bcryptjs');
